08ceb36ff4e82a97582bc9c9b83672ddab76d0b7
gotwarlost <kananthmail-github@yahoo.com>
2015-11-26 16:20:24 -0800

    Minor fixes, docs/tests still WIP

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
  Copyright 2012-2015, Yahoo Inc.
  Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
+/**
+ * @module Exports
+ */
 module.exports = {
     config: require('./lib/config'),
     cover: require('./lib/run-cover'),
