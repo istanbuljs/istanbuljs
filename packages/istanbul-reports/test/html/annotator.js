f490377dc61c4b58bd4241a29fe460ed5ca53d5d
Vincent Teo <vinteo@gmail.com>
2017-08-26 03:13:12 +1000

    fix: prevent branch highlighting from extending pass the end of a line (#80)

--- packages/istanbul-reports/test/html/annotator.js
+++ packages/istanbul-reports/test/html/annotator.js
@@ -32,5 +32,38 @@ describe('annotator', function () {
       });
       annotated.annotatedCode[0].should.not.match(/Cannot read property/);
     });
+
+    // see: https://github.com/istanbuljs/istanbuljs/pull/80
+    it('handles statement meta information with end column less than start column', function () {
+      var annotated = annotator.annotateSourceCode(getFixture('github-80a'), {
+        getSource: function () {
+          return '  var test = "test";';
+        }
+      });
+      annotated.annotatedCode[0].should
+        .equal('<span class="cstat-no" title="statement not covered" >  var test = "test";</span>');
+    });
+
+    // see: https://github.com/istanbuljs/istanbuljs/pull/80
+    it('handles function meta information with end column less than start column', function () {
+      var annotated = annotator.annotateSourceCode(getFixture('github-80b'), {
+        getSource: function () {
+          return '  function test () {};';
+        }
+      });
+      annotated.annotatedCode[0].should
+        .equal('<span class="fstat-no" title="function not covered" >  function test () {};</span>');
+    });
+
+    // see: https://github.com/istanbuljs/istanbuljs/pull/80
+    it('handles branch meta information with end column less than start column', function () {
+      var annotated = annotator.annotateSourceCode(getFixture('github-80c'), {
+        getSource: function () {
+          return 'if (cond1 && cond2) {';
+        }
+      });
+      annotated.annotatedCode[0].should
+        .equal('if (cond1 &amp;&amp; <span class="branch-0 cbranch-no" title="branch not covered" >cond2) {</span>');
+    });
   });
 });
