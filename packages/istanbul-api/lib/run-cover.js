2ce9d450d83172e7fb01e6dbd35321ab894a0d99
pangrr <pangrr89@gmail.com>
2017-10-01 17:01:04 -0400

    change coverageVar to another random value

--- packages/istanbul-api/lib/run-cover.js
+++ packages/istanbul-api/lib/run-cover.js
@@ -23,7 +23,7 @@ function getCoverFunctions(config, includes, callback) {
         reportingDir = path.resolve(config.reporting.dir()),
         reporter = new Reporter(config),
         excludes = config.instrumentation.excludes(true),
-        coverageVar = '$$cov_1506891400$$',
+        coverageVar = 'asdfcov' + new Date().getTime() + 'asdf`,
         instOpts = config.instrumentation.getInstrumenterOpts(),
         sourceMapStore = libSourceMaps.createSourceMapStore({}),
         instrumenter,
