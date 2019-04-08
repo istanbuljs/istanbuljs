fb642f76eee28973a7e1fd8b106d2575aa7d1f01
Luke Page <luke.a.page@gmail.com>
2019-04-08 21:05:04 +0200

    basic expanding

--- packages/istanbul-report-modern-html/src/summaryTableLine.js
+++ packages/istanbul-report-modern-html/src/summaryTableLine.js
@@ -20,41 +20,53 @@ function ShowPicture({ num }) {
     }
 }
 
-export default function SummaryTableLine({ metrics, file }) {
+export default function SummaryTableLine({ metrics, file, children }) {
+    const [toggled, setToggled] = React.useState(false);
+
     return (
-        <tr>
-            <td class={'file ' + metrics.statements.classForPercent}>
-                <a>{file}</a>
-            </td>
-            <td class={'pic ' + metrics.statements.classForPercent}>
-                <div class="chart">
-                    <ShowPicture num={metrics.statements.pct} />
-                </div>
-            </td>
-            <td class={'pct ' + metrics.statements.classForPercent}>
-                {metrics.statements.pct}%
-            </td>
-            <td class={'abs ' + metrics.statements.classForPercent}>
-                {metrics.statements.covered}/{metrics.statements.total}
-            </td>
-            <td class={'pct ' + metrics.branches.classForPercent}>
-                {metrics.branches.pct}%
-            </td>
-            <td class={'abs ' + metrics.branches.classForPercent}>
-                {metrics.branches.covered}/{metrics.branches.total}
-            </td>
-            <td class={'pct ' + metrics.functions.classForPercent}>
-                {metrics.functions.pct}%
-            </td>
-            <td class={'abs ' + metrics.functions.classForPercent}>
-                {metrics.functions.covered}/{metrics.functions.total}
-            </td>
-            <td class={'pct ' + metrics.lines.classForPercent}>
-                {metrics.lines.pct}%
-            </td>
-            <td class={'abs ' + metrics.lines.classForPercent}>
-                {metrics.lines.covered}/{metrics.lines.total}
-            </td>
-        </tr>
+        <>
+            <tr>
+                <td class={'file ' + metrics.statements.classForPercent}>
+                    {children ? (
+                        <a onClick={() => setToggled(!toggled)}>
+                            {toggled ? '-' : '+'}
+                        </a>
+                    ) : (
+                        false
+                    )}
+                    <a>{file}</a>
+                </td>
+                <td class={'pic ' + metrics.statements.classForPercent}>
+                    <div class="chart">
+                        <ShowPicture num={metrics.statements.pct} />
+                    </div>
+                </td>
+                <td class={'pct ' + metrics.statements.classForPercent}>
+                    {metrics.statements.pct}%
+                </td>
+                <td class={'abs ' + metrics.statements.classForPercent}>
+                    {metrics.statements.covered}/{metrics.statements.total}
+                </td>
+                <td class={'pct ' + metrics.branches.classForPercent}>
+                    {metrics.branches.pct}%
+                </td>
+                <td class={'abs ' + metrics.branches.classForPercent}>
+                    {metrics.branches.covered}/{metrics.branches.total}
+                </td>
+                <td class={'pct ' + metrics.functions.classForPercent}>
+                    {metrics.functions.pct}%
+                </td>
+                <td class={'abs ' + metrics.functions.classForPercent}>
+                    {metrics.functions.covered}/{metrics.functions.total}
+                </td>
+                <td class={'pct ' + metrics.lines.classForPercent}>
+                    {metrics.lines.pct}%
+                </td>
+                <td class={'abs ' + metrics.lines.classForPercent}>
+                    {metrics.lines.covered}/{metrics.lines.total}
+                </td>
+            </tr>
+            {toggled && children.map(child => <SummaryTableLine {...child} />)}
+        </>
     );
 }
