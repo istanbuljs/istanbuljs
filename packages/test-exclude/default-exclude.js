8ad5fd21ac3418447fa30f55817a77d92cb325f4
Watanabe Shinnosuke <snnskwtnb@gmail.com>
2019-06-11 09:42:24 +0900

    feat: ignore files under test**s** directories by default (#419)

--- packages/test-exclude/default-exclude.js
+++ packages/test-exclude/default-exclude.js
@@ -4,8 +4,8 @@ const devConfigs = ['ava', 'babel', 'jest', 'nyc', 'rollup', 'webpack'];
 
 module.exports = [
     'coverage/**',
-    'packages/*/test/**',
-    'test/**',
+    'packages/*/test{,s}/**',
+    'test{,s}/**',
     'test{,-*}.{js,cjs,mjs,ts}',
     '**/*{.,-}test.{js,cjs,mjs,ts}',
     '**/__tests__/**',
