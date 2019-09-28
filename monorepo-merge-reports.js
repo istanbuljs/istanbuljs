264db3568cb7f9840de678c6c69616106a45159a
Corey Farrell <git@cfware.com>
2019-09-28 14:54:05 -0400

    chore: Run nyc merge from each package directory
    This ensures source-map's are correctly processed for
    istanbul-lib-instrument, it seems they are not found if `nyc merge` is
    run from the monorepo root.

--- monorepo-merge-reports.js
+++ monorepo-merge-reports.js
@@ -13,17 +13,23 @@ rimraf.sync('.nyc_output');
 makeDir.sync('.nyc_output');
 // Merge coverage data from each package so we can generate a complete report
 glob.sync('packages/*/.nyc_output').forEach(nycOutput => {
+    const cwd = path.dirname(nycOutput);
     const { status, stderr } = spawnSync(
         'nyc',
         [
             'merge',
-            nycOutput,
+            '.nyc_output',
             path.join(
+                __dirname,
                 '.nyc_output',
-                path.basename(path.dirname(nycOutput)) + '.json'
+                path.basename(cwd) + '.json'
             )
         ],
-        { encoding: 'utf8', shell: true }
+        {
+            encoding: 'utf8',
+            shell: true,
+            cwd
+        }
     );
 
     if (status !== 0) {
