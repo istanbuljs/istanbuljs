4e366c4f868e7d1c5dd13b3b692c245d723ec0e6
Corey Farrell <git@cfware.com>
2019-10-01 10:38:15 -0400

    fix: Produce properly merged source-maps when inputSourceMap is provided

--- packages/istanbul-lib-instrument/test/already-instrumented.test.js
+++ packages/istanbul-lib-instrument/test/already-instrumented.test.js
@@ -3,10 +3,13 @@
 import { assert } from 'chai';
 import Instrumenter from '../src/instrumenter';
 
-function instrument(code) {
-    // XXX produceSourceMap: true produces an altered source-map for the second run.
-    const instrumenter = new Instrumenter({ produceSourceMap: false });
-    const result = instrumenter.instrumentSync(code, __filename);
+function instrument(code, inputSourceMap) {
+    const instrumenter = new Instrumenter({ compact: false });
+    const result = instrumenter.instrumentSync(
+        code,
+        __filename,
+        inputSourceMap
+    );
     return {
         code: result,
         coverageData: instrumenter.lastFileCoverage(),
@@ -17,6 +20,12 @@ function instrument(code) {
 const instrumented = instrument(`console.log('basic test');`);
 
 it('should not alter already instrumented code', () => {
-    const result = instrument(instrumented.code);
+    const result = instrument(instrumented.code, instrumented.sourceMap);
+    [instrumented, result].forEach(({ sourceMap }) => {
+        // XXX Ignore source-map difference caused by:
+        // https://github.com/babel/babel/issues/10518
+        delete sourceMap.mappings;
+        delete sourceMap.names;
+    });
     assert.deepEqual(instrumented, result);
 });
