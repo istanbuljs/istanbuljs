2cfacf2acecfd7485e421ddd4a228a31d8da421d
Sebastian Lopez <sebastian.lopez@klm.com>
2020-02-28 17:36:34 +0100

    Adjustment in indenting

--- packages/istanbul-reports/lib/lcovonly/index.js
+++ packages/istanbul-reports/lib/lcovonly/index.js
@@ -6,63 +6,63 @@
 const {ReportBase} = require('istanbul-lib-report');
 
 class LcovOnlyReport extends ReportBase {
-  constructor(opts) {
-    super();
-    this.file = opts.file || 'lcov.info';
-    this.projectRoot = opts.projectRoot || null;
-    this.contentWriter = null;
-  }
+    constructor(opts) {
+        super();
+        this.file = opts.file || 'lcov.info';
+        this.projectRoot = opts.projectRoot || null;
+        this.contentWriter = null;
+    }
 
-  onStart(root, context) {
-    this.contentWriter = context.writer.writeFile(this.file);
-  }
+    onStart(root, context) {
+        this.contentWriter = context.writer.writeFile(this.file);
+    }
 
-  onDetail(node) {
-    const fc = node.getFileCoverage();
-    const writer = this.contentWriter;
-    const functions = fc.f;
-    const functionMap = fc.fnMap;
-    const lines = fc.getLineCoverage();
-    const branches = fc.b;
-    const branchMap = fc.branchMap;
-    const summary = node.getCoverageSummary();
-    const path = require('path');
+    onDetail(node) {
+        const fc = node.getFileCoverage();
+        const writer = this.contentWriter;
+        const functions = fc.f;
+        const functionMap = fc.fnMap;
+        const lines = fc.getLineCoverage();
+        const branches = fc.b;
+        const branchMap = fc.branchMap;
+        const summary = node.getCoverageSummary();
+        const path = require('path');
 
-    writer.println('TN:'); //no test nam
-    writer.println('SF:' + (this.projectRoot ? path.relative(this.projectRoot, fc.path) : fc.path));
+        writer.println('TN:'); //no test nam
+        writer.println('SF:' + (this.projectRoot ? path.relative(this.projectRoot, fc.path) : fc.path));
 
-    Object.values(functionMap).forEach(meta => {
-      writer.println('FN:' + [meta.decl.start.line, meta.name].join(','));
-    });
-    writer.println('FNF:' + summary.functions.total);
-    writer.println('FNH:' + summary.functions.covered);
+        Object.values(functionMap).forEach(meta => {
+            writer.println('FN:' + [meta.decl.start.line, meta.name].join(','));
+        });
+        writer.println('FNF:' + summary.functions.total);
+        writer.println('FNH:' + summary.functions.covered);
 
-    Object.entries(functionMap).forEach(([key, meta]) => {
-      const stats = functions[key];
-      writer.println('FNDA:' + [stats, meta.name].join(','));
-    });
+        Object.entries(functionMap).forEach(([key, meta]) => {
+            const stats = functions[key];
+            writer.println('FNDA:' + [stats, meta.name].join(','));
+        });
 
-    Object.entries(lines).forEach(entry => {
-      writer.println('DA:' + entry.join(','));
-    });
-    writer.println('LF:' + summary.lines.total);
-    writer.println('LH:' + summary.lines.covered);
+        Object.entries(lines).forEach(entry => {
+            writer.println('DA:' + entry.join(','));
+        });
+        writer.println('LF:' + summary.lines.total);
+        writer.println('LH:' + summary.lines.covered);
 
-    Object.entries(branches).forEach(([key, branchArray]) => {
-      const meta = branchMap[key];
-      const {line} = meta.loc.start;
-      branchArray.forEach((b, i) => {
-        writer.println('BRDA:' + [line, key, i, b].join(','));
-      });
-    });
-    writer.println('BRF:' + summary.branches.total);
-    writer.println('BRH:' + summary.branches.covered);
-    writer.println('end_of_record');
-  }
+        Object.entries(branches).forEach(([key, branchArray]) => {
+            const meta = branchMap[key];
+            const {line} = meta.loc.start;
+            branchArray.forEach((b, i) => {
+                writer.println('BRDA:' + [line, key, i, b].join(','));
+            });
+        });
+        writer.println('BRF:' + summary.branches.total);
+        writer.println('BRH:' + summary.branches.covered);
+        writer.println('end_of_record');
+    }
 
-  onEnd() {
-    this.contentWriter.close();
-  }
+    onEnd() {
+        this.contentWriter.close();
+    }
 }
 
 module.exports = LcovOnlyReport;
