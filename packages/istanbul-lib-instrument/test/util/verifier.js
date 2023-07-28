03cea79f2ff47183c8d71d73b55f3bdaf49aa5f5
Alex Yang <himself65@outlook.com>
2023-07-28 08:32:24 -0700

    fix: code

--- packages/istanbul-lib-instrument/test/util/verifier.js
+++ packages/istanbul-lib-instrument/test/util/verifier.js
@@ -26,10 +26,19 @@ function annotatedCode(code) {
 }
 
 function getGlobalObject() {
-  return typeof globalThis === 'object' ? globalThis :
-    typeof window === 'object' ? window :
-      typeof self === 'object' ? self :
-        new Function('return this')();
+  if (typeof globalThis !== 'undefined') {
+    return globalThis;
+  }
+  if (typeof global !== 'undefined') {
+    return global;
+  }
+  if (typeof window !== 'undefined') {
+    return window;
+  }
+  if (typeof self !== 'undefined') {
+    return self;
+  }
+  return new Function('return this')();
 }
 
 class Verifier {
