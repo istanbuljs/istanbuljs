8b1baa8f45b6457ae2b467ddc5d4b6d9d9ea8690
Alex Yang <himself65@outlook.com>
2023-10-04 02:17:24 -0500

    fix: globalThis with CSP compatibility (#734)

--- packages/istanbul-lib-instrument/test/util/verifier.js
+++ packages/istanbul-lib-instrument/test/util/verifier.js
@@ -26,6 +26,22 @@ function annotatedCode(code) {
 }
 
 function getGlobalObject() {
+    if (typeof globalThis !== 'undefined') {
+        // eslint-disable-next-line no-undef
+        return globalThis;
+    }
+    if (typeof global !== 'undefined') {
+        // eslint-disable-next-line no-undef
+        return global;
+    }
+    if (typeof window !== 'undefined') {
+        // eslint-disable-next-line no-undef
+        return window;
+    }
+    if (typeof self !== 'undefined') {
+        // eslint-disable-next-line no-undef
+        return self;
+    }
     return new Function('return this')();
 }
 
